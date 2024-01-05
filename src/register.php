5c7a1e6bf675ca187c20a4b6c6e3d6196f0bc49d
Ajay Jagdale <129574084+ajay-jagdale@users.noreply.github.com>
2024-01-05 23:28:33 +0530

    change

--- src/register.php
+++ src/register.php
@@ -1,36 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
 
-<?php
-// Create a database connection
-$servername = "localhost";
-$username = "root";
-$password = "";   // Enter your password here
-$dbname = "my_db";
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Registration Form</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+</head>
 
-$conn = new mysqli($servername, $username, $password, $dbname);
+<body style="user-select: none;">
 
-// Check connection
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-}
+  <?php
+  // Create a database connection
+  $servername = "localhost";
+  $username = "root";
+  $password = "";   // Enter your password here
+  $dbname = "my_db";
 
-// Get form data
-$firstname = $_POST['firstname'];
-$lastname = $_POST['lastname'];
-$mobileno = $_POST['mobileno'];
-$email = $_POST['email'];
-$password = $_POST['password'];
+  $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Insert form data into database
-$sql = "INSERT INTO `users` (firstname, lastname, mobileno, email, password)
+  // Check connection
+  if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+  }
+
+  // Get form data
+  $firstname = $_POST['firstname'];
+  $lastname = $_POST['lastname'];
+  $mobileno = $_POST['mobileno'];
+  $email = $_POST['email'];
+  $password = $_POST['password'];
+
+  // Insert form data into database
+  $sql = "INSERT INTO `users` (firstname, lastname, mobileno, email, password)
 VALUES ('$firstname', '$lastname', '$mobileno', '$email', '$password')";
 
-if ($conn->query($sql) === TRUE) {
-  echo "<br><br><br><br><br><br><br><br><br><br><br>";
-  echo "<center><h1> Registration Successful! </h1></center>";
-} else {
-  echo "Error: " . $sql . "<br>" . $conn->error;
-}
+  if ($conn->query($sql) === TRUE) {
+    echo '<p class="h1 text-center" style="margin-top: 200px; color: #0d6efd"> Registration Successful! </p>';
+  } else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+  }
 
-// Close database connection
-$conn->close();
-?>
+  // Close database connection
+  $conn->close();
+  ?>
+
+
+</body>
+
+</html>
